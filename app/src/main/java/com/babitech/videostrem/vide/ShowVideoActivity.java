a1af9015318c86a2ff013d8dd484724612b41b0e
Babi Mumba <babimumba243@gmail.com>
2022-12-04 05:09:55 +0200

    add ewn file

--- app/src/main/java/com/babitech/videostrem/vide/ShowVideoActivity.java
+++ app/src/main/java/com/babitech/videostrem/vide/ShowVideoActivity.java
@@ -22,8 +22,11 @@ public class ShowVideoActivity extends AppCompatActivity {
         videoView = findViewById(R.id.vide_view);
         videoView.setMediaController(mediaController);
         mediaController.setAnchorView(videoView);
+
         String lien = "https://firebasestorage.googleapis.com/v0/b/videoshow-6801b.appspot.com/o/myvideos%2F1670119056829.mp4?alt=media&token=55180769-ad0a-401a-a0fd-ba9b2bd0905f";
-        Uri uri = Uri.parse(lien);
+        String lien2 = "https://d286.d2mefast.net/tb/d/a7/whitney_houston_i_look_to_you_h264_10418.mp4?play";
+
+        Uri uri = Uri.parse(lien2);
         videoView.setVideoURI(uri);
         videoView.requestFocus();
         videoView.start();
---
+++ app/src/main/java/com/babitech/videostrem/vide/StremActivity.java
@@ -0,0 +1,17 @@
+package com.babitech.videostrem.vide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+import com.babitech.videostrem.R;
+
+public class StremActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_video);
+
+    }
+}
